f05eea4af8a68131509dfc17fc77af0a7dff0755
Chuck Harmston <chuck@chuckharmston.com>
2011-05-31 12:39:08 -0500

    Adds custom admin site containing extra adminsettings views

---
+++ adminsettings/admin.py
@@ -0,0 +1,45 @@
+from copy import copy
+from functools import update_wrapper
+
+from django.contrib.admin.sites import AdminSite
+from django.template.response import TemplateResponse
+from django.views.decorators.cache import never_cache
+
+from adminsettings.conf import settings
+
+
+class SettingsAdminSite(AdminSite):
+
+    def get_urls(self):
+        from django.conf.urls.defaults import patterns, url, include
+
+        def wrap(view, cacheable=False):
+            def wrapper(*args, **kwargs):
+                return self.admin_view(view, cacheable)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+
+        # Admin-site-wide views.
+        urlpatterns = patterns('',
+            url(r'^settings/$',
+                wrap(self.settings),
+                name='settings'),
+        )
+        return urlpatterns
+
+    @never_cache
+    def settings(self, request):
+        import pdb; pdb.set_trace()
+        admin_settings = []
+        for setting in settings._obj_registry:
+            value = settings._registry[setting.key()]
+            admin_settings.append({
+                'class': setting,
+                'instance': setting(value),
+                'value': value
+            })
+        return TemplateResponse(request, ['adminsettings/index.html'], {
+            'title': 'Settings',
+            'admin_settings': admin_settings
+        })
+
+adminsettings_site = SettingsAdminSite('adminsettings')
