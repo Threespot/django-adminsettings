b76ebb3ffe6268ff981b03080ba515ef23117931
Chuck Harmston <chuck@chuckharmston.com>
2011-06-10 13:50:00 -0400

    Refactors registry to use a subclass of string containing extra properties to hold the setting object and model object, rather than requiring multiple registries.

--- adminsettings/conf.py
+++ adminsettings/conf.py
@@ -9,10 +9,14 @@ CACHE_NAME = 'adminsettings_cache'
 CACHE_TTL = 10
 
 
+class SettingValue(str):
+    setting_object = None
+    model_object = None
+
+
 class AdminSettingSite(object):
 
     def __init__(self):
-        self._obj_registry = []
         self._registry = {}
 
     def __iter__(self):
@@ -22,44 +26,47 @@ class AdminSettingSite(object):
         """
         Registers a given setting. If it has already been registered, this
         will raise AlreadyRegistered.
-
-        The configuration values are stored in the database, as instances of the
-        class that defines them. So, here:
-        admin_setting = the setting class
-        db_obj        = the stored instance the Django model
-        db_obj.value  = the stored instance of the setting class
         """
 
         # To prevent multiple DB lookups, let's run and evaluate the queryset,
         # which allows us to pass on the value to any sub-registrations (i.e. if
         # a list was passed to the function)
+
         if not setting_cache:
             setting_cache = AdminSetting.objects.all()
 
+        # Iterables containing setting objects can also be registered
         if hasattr(admin_setting, '__iter__'):
             for item in admin_setting:
                 self.register(item, setting_cache=setting_cache)
+
         else:
-            if admin_setting in self._obj_registry:
+
+            if admin_setting.key() in self._registry.keys():
                 raise AlreadyRegistered('The setting %s has already been '
                     'registered' % admin_setting.__name__)
-            self._obj_registry.append(admin_setting)
-            setting_name = admin_setting.key()
+
             try:
-                db_obj = setting_cache.get(name=admin_setting.name)
-                self._registry[setting_name] = db_obj.value.value
+                model_object = setting_cache.get(name=admin_setting.name)
+                value = SettingValue(model_object.value.value)
+                value.model_object = model_object
             except AdminSetting.DoesNotExist:
-                self._registry[setting_name] = admin_setting.default
+                value = SettingValue(admin_setting.default)
+                value.model_object = None
+            value.setting_object = admin_setting
+
+            self._registry[admin_setting.key()] = value
+
 
     def unregister(self, admin_setting):
         """
         Unregisters a given setting. If it has not been registered, this will
         raise NotRegistered.
         """
-        if admin_setting not in self._obj_registry:
+        if admin_setting.key() not in self._registry.keys:
             raise NotRegistered('The setting %s cannot be unregistered as it '
                 'has not been registered' % admin_setting.__name__)
-        self._obj_registry.remove(admin_setting)
+        del self._registry[admin_setting.key()]
 
 
 settings = cache.get(CACHE_NAME)
